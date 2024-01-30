718f4b29304a047af66c5c6ffcf159313f4909e0
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2024-01-31 00:26:00 +0600

    task-1 added

---
+++ task1.js
@@ -0,0 +1,10 @@
+var amount = 1000;
+var orangePrice=330;
+var applePrice= 300;
+
+totalPrice = orangePrice+applePrice;
+returnAmount =amount - totalPrice;
+console.log('Total price to buy apple and orange =', totalPrice);
+console.log('The amount i paid to the shopkeeper =', amount);
+
+console.log('The shopkeeper will return = ', returnAmount);
---
+++ variable.js
@@ -0,0 +1,54 @@
+/**
+ * Variable = vary + able.
+ * 5 things to declare a variable.
+ * variable_type variable_name = value ;
+ * var age = 39 ;
+ *
+ *  */
+// Data Type = Number;
+var age = 39;
+console.log(typeof 'Your age is =', age);
+
+// Data Type = String.
+var name = "Shanto";
+console.log(typeof name);
+var address ="House no: 87-88, Road no: 7, Block: B"
+console.log(address);
+
+// Data Type = Boolean.
+var isRich = true;
+console.log(typeof isRich);
+
+
+// For Declaring a variable no keywords are allowed.
+// three types of case:-
+// 1. snake case :- Aladin_madafaka_yahoo
+// 2. Camel case :- aladinMadafakaYahoo
+// 3. pascal Case :- AladinMadafakaYahoo
+
+
+var orangePrice = 30;
+var chocolate =0.5;
+var applePrice =parseFloat('180.76'); //to convert from string to number or number to string we need to use parseint/parsefloat.
+console.log(orangePrice + applePrice);
+
+
+console.log(typeof orangePrice);
+
+// operations:
+
+var eggPrice = 130.45;
+var onionPrice = 200.30;
+var oilPrice = parseFloat('340.75');
+
+
+var totalPrice= eggPrice+onionPrice+oilPrice;
+console.log('total price =', totalPrice);
+console.log(eggPrice*onionPrice*oilPrice);
+console.log(eggPrice-onionPrice-oilPrice);
+console.log(eggPrice/onionPrice/oilPrice);
+console.log(10/4);
+console.log(10%4);
+
+
+
