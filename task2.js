be3e55d9356b9b640845b6a19e71ebf626bacb60
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2024-01-31 00:39:22 +0600

    task2 added

---
+++ task2.js
@@ -0,0 +1,21 @@
+console.log('Result of a student');
+console.log('Subject name and marks :');
+
+var marksOfMathematics= 88;
+var marksOfBangla= 70.55;
+var marksOfBiology= 81.96;
+var marksOfChemistry= 70;
+var marksOfPhysics= 91.45;
+
+console.log(marksOfMathematics);
+console.log(marksOfBangla);
+console.log(marksOfBiology);
+console.log(marksOfChemistry);
+console.log(marksOfPhysics);
+
+var totalMarks= marksOfMathematics+marksOfBangla+marksOfBiology+marksOfChemistry+marksOfPhysics;
+
+console.log('total number obtained =', totalMarks);
+
+average= totalMarks/5;
+console.log('average number of the student =', average);
